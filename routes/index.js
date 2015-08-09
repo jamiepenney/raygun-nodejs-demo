93d30147ad90f19dec3428f5050138a37285298c
Jamie Penney <jamie@jamiepenney.co.nz>
2015-08-09 21:51:58 +1200

    Add failure route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/fail', function(req, res, next) {
+  throw new Error("Danger, Will Robinson!");
+});
+
 module.exports = router;
